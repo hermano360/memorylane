2cea2b4744089d5e1883a30d3d86c725509c90fb
Herminio Garcia <herminio@budsy.io>
2018-01-20 16:23:14 -0800

    heroku

--- herminio/server.js
+++ herminio/server.js
@@ -7,6 +7,10 @@ app.use(express.static('../'))
 // Call the multerImpl and pass in app state to it
 require('./src-server/multerImpl')(app)
 
+app.get('/', function (req, res) {
+  res.redirect('/upload')
+})
+
 module.exports = app.listen(app.get('port'), () => {
   console.log('Express server listening on port ' + app.get('port'))
 })
